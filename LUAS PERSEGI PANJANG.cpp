88664e597fac64b88699a76eb5f5545535502d14
Suhendar <45907948+Suhendarprogrammer@users.noreply.github.com>
2018-12-31 00:17:25 +0700

    Add files via upload

---
+++ LUAS PERSEGI PANJANG.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+	int luas,p,l;
+	cout<<"*********PROGRAM MENGHITUNG LUAS PERSEGI PANJANG*********\n";
+	cout<<"=========================================================\n";
+	cout<<"\nMasukkan Panjang Persegi : ";
+	cin>>p;
+	cout<<"\nMasukkan Luas Persegi : ";
+	cin>>l;
+	luas=p*l;
+	cout<<"Maka, Luas Persegi Panjang Adalah : "<<luas<<"\n\n";
+	return 0;
+	
+}
